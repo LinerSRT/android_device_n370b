6c40bd9d2c113b6793fa2371c7ef70d6403c3d57
LinerSRT <serinity320@gmail.com>
2019-02-13 14:08:09 +0000

    rebase patch script, add DownProvider patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 cd ../../../..
-cd system/core
-git apply -v ../../device/doogee/n370b/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v ../../device/doogee/n370b/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
-git apply -v ../../device/doogee/n370b/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
-cd ../..
-cd bionic
-git apply -v ../device/doogee/n370b/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
-cd ..
-cd system/sepolicy
-git apply -v ../../device/doogee/n370b/patches/0001-sepolicy-v29-patch.patch
-cd ../..
-cd frameworks/av
-git apply -v ../../device/doogee/n370b/patches/0006-fix-access-wvm-to-ReadOptions.patch
-git apply -v ../../device/doogee/n370b/patches/0007-Disable-usage-of-get_capture_position.patch
-git apply -v ../../device/doogee/n370b/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
-git apply -v ../../device/doogee/n370b/patches/0009-add-mtk-color-format-support.patch
-git apply -v ../../device/doogee/n370b/patches/0013-fix_video_autoscaling.patch
-cd ../..
-cd system/netd
-git apply -v ../../device/doogee/n370b/patches/NETD-mtl_patch-p1.patch
-cd ../..
+SOURCE_DIR=$PWD
+cd $SOURCE_DIR/system/core
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
+cd $SOURCE_DIR/bionic
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
+cd $SOURCE_DIR/system/sepolicy
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0001-sepolicy-v29-patch.patch
+cd $SOURCE_DIR/frameworks/av
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0006-fix-access-wvm-to-ReadOptions.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0007-Disable-usage-of-get_capture_position.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0009-add-mtk-color-format-support.patch
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0013-fix_video_autoscaling.patch
+cd $SOURCE_DIR/system/netd
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/NETD-mtl_patch-p1.patch
+cd $SOURCE_DIR/packages/providers/DownloadProvider
+git apply -v $SOURCE_DIR/device/doogee/n370b/patches/0001-revert-for-DP.patch
+cd $SOURCE_DIR
 
